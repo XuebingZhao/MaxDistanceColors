e06af239b0e9fd0d2074d7f0027d65a48a6c0610
XuebingZhao <zhao1xue2bing@gmail.com>
2025-02-24 08:58:53 +0800

    refactor(results): 新增DIN99d, Oklab计算结果

--- results/batch_run.py
+++ results/batch_run.py
@@ -33,3 +33,67 @@ if __name__ == '__main__':
             for row in result:
                 new_row = [row[0], row[1], *row[2]]
                 writer.writerow(new_row)
+
+    # # sRGB_DIN99d
+    # result = [[0, 0, [None]]]
+    # for n in np.arange(1, 52, 1):
+    #     csv_file = "sRGB_DIN99d.csv"
+    #     hexs, de = multi_run(n+1, [1, 1, 1], color_space='sRGB',
+    #                          uniform_space='DIN99d', quality='medium', num_runs=32, show=False)
+    #     hexs = [f"'{h}'" for h in hexs]
+    #     result.append([n+1, de, hexs])
+    #
+    #     with open(csv_file, "w", newline='') as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(["Number of Colors", "Delta E", "Colors"])
+    #         for row in result:
+    #             new_row = [row[0], row[1], *row[2]]
+    #             writer.writerow(new_row)
+    #
+    # # sRGB_x_Japan2001Coated_DIN99d
+    # result = [[0, 0, [None]]]
+    # for n in np.arange(1, 52, 1):
+    #     csv_file = "sRGB_x_Japan2001Coated_DIN99d.csv"
+    #     hexs, de = multi_run(n+1, [1, 1, 1], color_space='CMYK',
+    #                          uniform_space='DIN99d', quality='medium', num_runs=32, show=False)
+    #     hexs = [f"'{h}'" for h in hexs]
+    #     result.append([n+1, de, hexs])
+    #
+    #     with open(csv_file, "w", newline='') as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(["Number of Colors", "Delta E", "Colors"])
+    #         for row in result:
+    #             new_row = [row[0], row[1], *row[2]]
+    #             writer.writerow(new_row)
+    #
+    # # sRGB_Oklab
+    # result = [[0, 0, [None]]]
+    # for n in np.arange(1, 52, 1):
+    #     csv_file = "sRGB_Oklab.csv"
+    #     hexs, de = multi_run(n+1, [1, 1, 1], color_space='sRGB',
+    #                          uniform_space='Oklab', quality='medium', num_runs=32, show=False)
+    #     hexs = [f"'{h}'" for h in hexs]
+    #     result.append([n+1, de, hexs])
+    #
+    #     with open(csv_file, "w", newline='') as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(["Number of Colors", "Delta E", "Colors"])
+    #         for row in result:
+    #             new_row = [row[0], row[1], *row[2]]
+    #             writer.writerow(new_row)
+    #
+    # # sRGB_x_Japan2001Coated_DIN99d
+    # result = [[0, 0, [None]]]
+    # for n in np.arange(1, 52, 1):
+    #     csv_file = "sRGB_x_Japan2001Coated_Oklab.csv"
+    #     hexs, de = multi_run(n+1, [1, 1, 1], color_space='CMYK',
+    #                          uniform_space='Oklab', quality='medium', num_runs=32, show=False)
+    #     hexs = [f"'{h}'" for h in hexs]
+    #     result.append([n+1, de, hexs])
+    #
+    #     with open(csv_file, "w", newline='') as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(["Number of Colors", "Delta E", "Colors"])
+    #         for row in result:
+    #             new_row = [row[0], row[1], *row[2]]
+    #             writer.writerow(new_row)
